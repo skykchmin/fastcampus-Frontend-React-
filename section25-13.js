f651907330da01214b471684a155ccd673c6a66e
skykchmin <43941493+skykchmin@users.noreply.github.com>
2020-03-07 01:13:03 +0900

    25-13. 객체
    25-13. 객체
    
    벨로퍼트와 함께하는 모던 자바스크립트
    [200307]

--- section25-13.js
+++ section25-13.js
@@ -1,2 +1,39 @@
-// 비구조화 할당
-// 객체 구조분해 : 객체에서 특정값들을 추출해내는것
+// 06. 객체
+// 객체는 우리가 변수 혹은 상수를 사용하게 될 때 하나의 이름에 여러 종류의 값을 넣을 수 있게 해줍니다.
+
+// 객체 선언 방식: 키 : 원하는 값
+
+const dog = {
+  name: '멍멍이',
+  age: 2
+};
+
+console.log(dog.name);
+console.log(dog.age);
+
+
+const ironMan = {
+  name: '토니 스타크',
+  actor: '로버트 다우니 주니어',
+  alias: '아이언맨'
+};
+
+const captainAmerica = {
+  name: '스티븐 로저스',
+  actor: '크리스 에반스',
+  alias: '캡틴 아메리카'
+};
+
+console.log(ironMan);
+console.log(captainAmerica);
+
+function print(hero) {
+  const text = `${hero.alias}(${hero.name}) 역할을 맡은 배우는 ${
+    hero.actor
+  } 입니다.`;
+  console.log(text);
+}
+
+print(ironMan);
+print(captainAmerica);
+  
