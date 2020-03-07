a50f3f825252092251123e06704a946b662e9951
skykchmin <43941493+skykchmin@users.noreply.github.com>
2020-03-07 23:51:04 +0900

    section26-6. 비구조화 할당
    section26-6. 비구조화 할당
    
    벨로퍼트와 함께하는 모던 자바스크립트
    [200307]

--- section26-6.js
+++ section26-6.js
@@ -46,3 +46,85 @@ const animal = {
   console.log(nickname); // 멍멍이
 
 //   animal.name 값을 nickname 값에 담고 있는데요, 이름이 같다면 그냥 우리가 이전에 배웠던 대로 비구조화 할당을 쓰면 되는데 지금은 이름이 서로 다릅니다.
+
+// 배열 비구조화 할당
+// 비구조화 할당은 객체에만 할 수 있는 것이 아닙니다. 배열에서 할 수 있어요.
+
+const array = [1, 2];
+const [one, two] = array;
+
+console.log(one);
+console.log(two);
+
+// 객체 비구조화 할당과 마찬가지로, 기본값 지정이 가능합니다.
+
+const array = [1];
+const [one, two = 2] = array;
+
+console.log(one);
+console.log(two);
+
+// 깊은 값 비구조화 할당
+// 객체의 깊숙한 곳에 들어있는 값을 꺼내는 방법을 알아봅시다.
+
+
+const deepObject = {
+  state: {
+    information: {
+      name: 'velopert',
+      languages: ['korean', 'english', 'chinese']
+    }
+  },
+  value: 5
+};
+
+// name, languages, value 값들을 밖으로 꺼내주고 싶다면 어떻게 해야 할까요? 이럴땐 두가지 해결 방법이 있는데요, 
+// 1) 비구조화 할당 문법을 두번 사용하는 것입니다.
+
+const deepObject = {
+  state: {
+    information: {
+      name: 'velopert',
+      languages: ['korean', 'english', 'chinese']
+    }
+  },
+  value: 5
+};
+
+const { name, languages } = deepObject.state.information;
+const { value } = deepObject;
+
+const extracted = {
+  name,
+  languages,
+  value
+};
+
+console.log(extracted); // {name: "velopert", languages: Array[3], value: 5}
+
+//2) 한번에 모두 추출하는 방법을 알아보겠습니다.
+
+const deepObject = {
+  state: {
+    information: {
+      name: 'velopert',
+      languages: ['korean', 'english', 'chinese']
+    }
+  },
+  value: 5
+};
+
+const {
+  state: {
+    information: { name, languages }
+  },
+  value
+} = deepObject;
+
+const extracted = {
+  name,
+  languages,
+  value
+};
+
+console.log(extracted);
